ae6c6718f593728e25ace3ad1706403f844c0575
handfilm <handfilm.ai@gmail.com>
2026-08-25 05:41:51 +0600

    update

--- android/app/src/main/java/com/handfilm/bloom/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.handfilm.bloom;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.handfilm.bloom',
-  appName: 'bloom',
-  webDir: 'out'
-};
-
-export default config;
